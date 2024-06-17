e0b5d046980fc4a1e3ad28259f9e225d4c826f14
hmin___ <44727850+shinhm1@users.noreply.github.com>
2024-06-17 09:43:24 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17780 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -15,12 +15,15 @@ public class Main {
         for (int i = 0; i < hash.length; i++) {
             hash[i] = str.charAt(i) - 'a' + 1;
             hash[i] *= r;
-            r *= 31;
 
-            H += hash[i];
+            // '해시 함수의 정의에서 유한한 범위의 출력을 가져야 한다고 했으니 적당히 큰 수 M으로 나눈다.'
+            r = (r * 31) % 1234567891;
+            H = (H + hash[i]) % 1234567891;
+            // 1 <= L <= 50인 경우 값이 너무 커질 수 있음
+            // 따라서 해시값을 계속 mod 연산해 크기가 커지는 것을 방지해야 함
+
         }
 
-        H %= 1234567891;
         System.out.println(H);
         sc.close();
     }
