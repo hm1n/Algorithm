910762f17af1a04a98c92cba3f3f7e647ce4c659
hmin___ <44727850+shinhm1@users.noreply.github.com>
2024-06-19 11:17:54 +0900

    [Bronze II] Title: 막대기, Time: 820 ms, Memory: 115180 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,25 +1,35 @@
 import java.util.*;
+import java.util.Stack;
+
+/** [백준] 17608. 막대기 */
 
 public class Main {
+    /* 2. 스택을 이용한 풀이 */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int n; // 막대기 총 개수
+        int cnt; // 보이는 막대기 개수
+        int maxHeight; // 보이는 막대기 최대 높이
+
+        n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            int tmp = sc.nextInt();
+            stack.push(tmp);
         }
 
-        int h = arr[n - 1];
-        int cnt = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (h < arr[i]) {
+        cnt = 1;
+        maxHeight = stack.peek();
+        while (!stack.isEmpty()) {
+            int tmp = stack.pop();
+
+            if (tmp > maxHeight) {
                 cnt++;
-                h = arr[i];
+                maxHeight = tmp;
             }
         }
-
         System.out.println(cnt);
-        sc.close();
     }
 }
