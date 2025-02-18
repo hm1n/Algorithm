cafa6213130f0d26b86992a686070ae541b8307f
hmin___ <44727850+hminnnnn@users.noreply.github.com>
2025-02-18 17:15:56 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+arr = []
+
+for i in range(k):
+  num = int(input())
+
+  if (num != 0):
+    arr.append(num)
+  else:
+    arr.pop()
+
+print(sum(arr))
