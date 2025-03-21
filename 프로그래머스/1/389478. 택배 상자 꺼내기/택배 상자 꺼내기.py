d1ac89c2e4b2d22e4f2f6c369049a44f7e284437
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-03-21 20:35:35 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,40 +1,12 @@
-def round(n, k):
-    if (n % k > 0):
-        return n // k + 1
-    return n // k
-
 def solution(n, w, num):
-    arr = [[0] * w for _ in range(round(n, w))]
-
-    k = 1
-    
-    for i in range(round(n, w)):
-        for j in range(w):
-            if (i % 2 == 0):
-              arr[i][j] = k
-            else:
-              arr[i][w - j - 1] = k
-            k += 1
-            if (k > n):
-                break
-    
-    x = (num - 1) // w
-    
-    if (num % w == 0):
-        y = w - 1
-        if (x % 2 == 1):
-            y = 0
-    else:
-        y = num % w - 1
-        if (x % 2 == 1):
-            y = w - y - 1
-    
     cnt = 0
-    while (x < round(n, w)):
-        if (arr[x][y] == 0):
-            break
-        cnt += 1
-        x += 1
+    
+    m1 = num % (2 * w)
+    m2 = (2 * w - m1 + 1) % (2 * w)
+    
+    for i in range(num, n + 1):
+        if (i % (2 * w) in [m1, m2]):
+            cnt += 1
     
     return cnt
         
