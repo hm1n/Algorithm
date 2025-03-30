c388fd3b2db4cf0c33b785dc63ee3954abdb2da3
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-03-30 16:08:38 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 788 ms, Memory: 41728 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,41 +1,42 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().strip().split())
-
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-ch = [[0] * M for _ in range(N)]
+n, m = map(int, input().split())
 
 campus = []
-Q = deque()
+visited = [[0] * m for _ in range(n)]
+queue = deque()
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-for i in range(N):
-  campus.append(list(map(str, sys.stdin.readline().strip())))
+# 캠퍼스 입력
+for i in range(n):
+  campus.append(list(map(str, input().strip())))
 
-  for j in range(len(campus[i])):
-    if campus[i][j] == 'I':
-      Q.append([i, j])
-      ch[i][j] = 1
+  for j in range(m):  
+    if (campus[i][j] == 'I'):
+      queue.append([i, j])
+      visited[i][j] = 1
+      
 
+# 캠퍼스 탐색
 answer = 0
+while (queue):
+  for _ in range(len(queue)):
+    x, y = queue.popleft()
 
-while Q:
-  for _ in range(len(Q)):
-    r, c = Q.popleft()
-    
     for i in range(4):
-      nr, nc = r + dr[i], c + dc[i]
+      nx, ny = x + dx[i], y + dy[i]
 
-      if 0 <= nr < N and 0 <= nc < M and ch[nr][nc] == 0 and campus[nr][nc] != 'X':
-        if campus[nr][nc] == 'P':
+      if (0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and campus[nx][ny] != 'X'):
+        if (campus[nx][ny] == 'P'):
           answer += 1
+          
+        visited[nx][ny] = 1
+        queue.append([nx, ny])
 
-        ch[nr][nc] = 1
-        Q.append([nr, nc])
-
-if answer:
+if (answer):
   print(answer)
-
 else:
   print('TT')
