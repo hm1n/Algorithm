cf474852c86135cee81627ba2feb2d40805100c3
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-07-15 23:28:51 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,9 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    
+    answer += my_string.slice(0, s)
+    answer += overwrite_string
+    answer += my_string.slice(s + overwrite_string.length)
+    
+    return answer;
+}
