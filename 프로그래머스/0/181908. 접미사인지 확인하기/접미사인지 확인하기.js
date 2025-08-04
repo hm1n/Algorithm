73bfc28ce951305eef594f01f3c8fb3bc0715a2f
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-08-04 19:48:00 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,16 +1,3 @@
 function solution(my_string, is_suffix) {
-    let flag = 0;
-    let idx = 0;
-    
-    while (1) {
-        const find_idx = my_string.indexOf(is_suffix, idx)
-        const suffix = my_string.slice(find_idx)    
-        
-        if (is_suffix === suffix) flag = 1
-        if (find_idx === -1) break
-        idx = find_idx + 1
-        
-    }
-    
-    return flag;
+    return my_string.endsWith(is_suffix) ? 1 : 0
 }
