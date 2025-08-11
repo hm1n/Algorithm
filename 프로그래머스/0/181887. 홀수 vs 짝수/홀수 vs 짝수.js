82dfd8a9395fedb07d0f92117e32e4eb227fb4d5
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-08-11 19:44:40 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,6 +1,10 @@
 function solution(num_list) {
-    const even = num_list.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b);
-    const odd = num_list.filter((_, i) => i % 2 === 1).reduce((a, b) => a + b);
+    let even = 0;
+    let odd = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        i % 2 === 0 ? even += num_list[i] : odd += num_list[i];
+    }
     
     return even < odd ? odd : even;
 }
