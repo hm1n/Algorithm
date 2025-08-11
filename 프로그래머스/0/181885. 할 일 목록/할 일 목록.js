ab45569a99379ebeddc9023cbee0884504b8f47e
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-08-11 19:49:49 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,8 +1,3 @@
 function solution(todo_list, finished) {
-    var answer = [];
-    
-    finished.forEach((e, i) => {
-        if (!e) answer.push(todo_list[i])
-    })
-    return answer;
+    return todo_list.filter((_, i) => !finished[i]);
 }
