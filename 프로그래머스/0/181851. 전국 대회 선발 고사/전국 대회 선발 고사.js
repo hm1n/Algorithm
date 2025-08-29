a5339b221a223ccb5e6197da476ba369e0eb3bed
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-08-29 18:01:02 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,13 +1,4 @@
 function solution(rank, attendance) {
-    const rankMap = new Map();
-    
-    rank.forEach((e, i) => {
-        if (attendance[i]) {
-            rankMap.set(e, i);
-        }
-    })
-    
-    const winner = [...rankMap.entries()].sort((a, b) => a[0] - b[0]).slice(0, 3);
-    console.log(winner)
-    return 10000 * winner[0][1] + 100 * winner[1][1] + winner[2][1];
+    const [a, b, c] = rank.map((e, i) => [e, i]).filter((_, i) => attendance[i]).sort((a, b) => a[0] - b[0]);
+    return 10000 * a[1] + 100 * b[1] + c[1];
 }
