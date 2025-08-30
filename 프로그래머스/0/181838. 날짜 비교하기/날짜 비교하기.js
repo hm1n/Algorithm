831d2377a934118b8b7fe20f7d77e6ad6959eb88
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-08-30 23:46:25 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,10 @@
+function solution(date1, date2) {
+    for (let i = 0; i < 3; i++) {
+        if (date1[i] > date2[i]) {
+            return 0;
+        } else if (date1[i] < date2[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
