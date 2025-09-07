8f66f0aaec3786c249c8a32d3ffeedbcbd8901a4
hmin___ <44727850+hm1n@users.noreply.github.com>
2025-09-07 19:21:12 +0900

    [level 1] Title: 체육복, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.19 ms
+메모리: 33.4 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 05일 16:28:01
+2025년 09월 07일 19:21:08
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,24 +1,22 @@
 function solution(n, lost, reserve) {
-    const std = Array(n).fill(1);
     var answer = 0;
+    const student = Array(n).fill(1);
     
-    lost.forEach((e) => std[e - 1]--)
-    reserve.forEach((e) => std[e - 1]++)
+    lost.forEach((e, i) => student[e - 1]--);
+    reserve.forEach((e, i) => student[e - 1]++);
     
     for (let i = 0; i < n; i++) {
-        if (std[i] === 0 && std[i - 1] === 2) {
-            std[i]++;
-            std[i - 1]--;
+        if (student[i] === 0) {
+            if (student[i - 1] === 2) {
+                student[i - 1]--;
+                student[i]++;
+            } else if (student[i + 1] === 2) {
+                student[i + 1]--;
+                student[i]++;
+            }
         }
-        else if (std[i] === 0 && std[i + 1] === 2) {
-            std[i]++;
-            std[i + 1]--;
-        }
-    }
-
-    for (let keys of std) {
-        if (keys) answer++;
     }
     
+    student.forEach((e) => {if (e > 0) answer++;});
     return answer;
 }
