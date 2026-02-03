821d0881313a1747359b5f3b870924d814cb62aa
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-02-03 11:40:41 +0900

    [Bronze II] Title: 나머지, Time: 84 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -3,18 +3,16 @@ const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").trim().split(/\r?\n/);
 
 function solution(input) {
-    const map = new Map();
-    let count = 0;
+    const set = new Set();
 
     for (let i = 0; i < 10; i++) {
         const num = Number(input[i]);
-        if (!map.get(num % 42)) {
-            map.set(num % 42, 1);
-            count++;
+        if (!set.has(num % 42)) {
+            set.add(num % 42, 1);
         }
     }
 
-    console.log(count);
+    console.log(set.size);
 }
 
 solution(input);
