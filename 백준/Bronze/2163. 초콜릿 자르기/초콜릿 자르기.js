a256d8aa94dac9d8e83df1240289a808c7f25c78
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-02-17 16:50:40 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.js
@@ -0,0 +1,11 @@
+/** @format */
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split(/\r?\n/);
+
+function solution(input) {
+    const [N, M] = input[0].split(" ").map(Number);
+
+    console.log(N * M - 1);
+}
+
+solution(input);
