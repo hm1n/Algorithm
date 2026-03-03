f47486577f8314c5bbaced4059782a3fbda735e3
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-03-03 15:59:48 +0900

    [Silver V] Title: 나이순 정렬, Time: 356 ms, Memory: 47752 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,24 @@
+/** @format */
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split(/\r?\n/);
+
+function solution(input) {
+    const N = Number(input[0]);
+    const arr = [];
+    let ans = "";
+
+    for (let i = 0; i < N; i++) {
+        const [age, name] = input[i + 1].split(" ");
+        arr.push([Number(age), name]);
+    }
+
+    arr.sort((a, b) => a[0] - b[0]);
+
+    arr.forEach((e) => {
+        ans += `${e[0]} ${e[1]}\n`;
+    });
+
+    console.log(ans.trim());
+}
+
+solution(input);
