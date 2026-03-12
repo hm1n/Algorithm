e88fb04d417ccf1d9b3e097ef8577d860f2ad66a
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-03-12 16:13:41 +0900

    [level 2] Title: 전화번호 목록, Time: 273.86 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,17 @@
+function solution(phone_book) {
+    const H = new Set();
+    phone_book = phone_book.sort((a, b) => a.length - b.length);
+    
+    for (let e of phone_book) {
+        for (let i = 1; i <= e.length; i++) {
+            const str = e.slice(0, i);
+            
+            if (H.has(str)) {
+                return false;
+            }
+        }
+        H.add(e);
+    };
+    
+    return true;
+}
