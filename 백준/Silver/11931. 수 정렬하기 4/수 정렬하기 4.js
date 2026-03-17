ee7ca1d59f3e04b60b0cd31c3c11125d3a76cfac
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-03-17 18:29:55 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1240 ms, Memory: 229660 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.js
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.js
@@ -1,21 +1,15 @@
 /** @format */
-
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").trim().split(/\r?\n/);
 
-const N = Number(input[0]);
-const arr = [];
-let answer = "";
-
-for (let i = 1; i < input.length; i++) {
-    arr.push(Number(input[i]));
+function solution(input) {
+    input.shift();
+    console.log(
+        input
+            .map(Number)
+            .sort((a, b) => b - a)
+            .join("\n"),
+    );
 }
 
-arr.sort((a, b) => b - a);
-
-for (let i = 0; i < arr.length; i++) {
-    answer += arr[i];
-    answer += "\n";
-}
-
-console.log(answer);
+solution(input);
