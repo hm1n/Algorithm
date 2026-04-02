c343987d360cc64df3830b33c498c78a9cf8437e
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-04-02 17:09:38 +0900

    [Silver IV] Title: 토너먼트, Time: 100 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.js
@@ -0,0 +1,17 @@
+/** @format */
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split(/\r?\n/);
+
+function solution(input) {
+    let [N, a, b] = input[0].split(" ").map(Number);
+    let ans = 0;
+
+    while (a !== b) {
+        a = Math.floor((a + 1) / 2);
+        b = Math.floor((b + 1) / 2);
+        ans++;
+    }
+    console.log(ans);
+}
+
+solution(input);
