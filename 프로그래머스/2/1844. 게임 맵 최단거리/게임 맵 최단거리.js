5dfbfd8001dfd1de4b00dd598f6d8bae19de016c
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-04-03 03:04:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.68 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,38 @@
+function solution(maps) {
+    const Q = [];
+    const move = [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1],
+    ];
+    
+    const maxX = maps.length;
+    const maxY = maps[0].length;
+    const visited = Array.from({length: maxX}, () => new Array(maxY).fill(0));
+
+    Q.push([0, 0]);
+
+    while (Q.length) {
+        const [x, y] = Q.shift();
+
+        for (let m of move) {
+            const dx = x + m[0];
+            const dy = y + m[1];
+
+            if (0 <= dx && dx < maxX && 0 <= dy && dy < maxY) {
+                if (!visited[dx][dy] && maps[dx][dy]) {
+                    Q.push([dx, dy]);
+                    visited[dx][dy] = 1;
+                    maps[dx][dy] = maps[x][y] + 1;
+                }
+            }
+        }
+    }
+    
+    if (visited[maxX - 1][maxY - 1]) {
+        return maps[maxX - 1][maxY - 1];
+    } else {
+        return -1;
+    }
+}
