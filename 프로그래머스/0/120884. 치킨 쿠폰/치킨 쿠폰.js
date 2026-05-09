0e750cb94b2a9b1785b0659f911d131edebb908f
hmin___ <44727850+hm1n@users.noreply.github.com>
2026-05-10 02:04:56 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,11 @@
+function solution(chicken) {
+    var answer = 0;
+    
+    while (chicken >= 10) {
+        answer += Math.floor((chicken) / 10);
+        
+        chicken = Math.floor(chicken / 10) + chicken % 10;
+    }
+    
+    return answer;
+}
